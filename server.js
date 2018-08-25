5f4194c0b4a628883d5ecc7e8f90db5e17d12b5c
YangMMO <11436052@qq.com>
2018-08-25 14:09:06 +0800

    push

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ if (cluster.isMaster) {
         cluster.fork();
       }
 
-      if (info.error.date > errDate + 10000) {
-        errDate = info.err.date;
+      if (info.error[date] > errDate + 10000) {
+        errDate = info.err[date];
         console.log('err: ' + errDate)
         // mail.send('[服务器异常]' + err.toString(), err.stack)
       }
