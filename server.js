8240327620f0e99e9b51b1f117d54278fbf8c249
YangMMO <11436052@qq.com>
2018-08-25 12:47:19 +0800

    push

--- server.js
+++ server.js
@@ -1,19 +1,58 @@
 'use strict'
 
-const http = require('http');
+// const http = require('http');
 
-const port = 80;
+// const port = 80;
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
 
-	for(var key in require.cache){
-		if(!key.includes('node_modules')){
-			delete require.cache[key];
-		}
-	}
-	require('./router').router(req,res);
-});
+// 	for(var key in require.cache){
+// 		if(!key.includes('node_modules')){
+// 			delete require.cache[key];
+// 		}
+// 	}
+// 	require('./router').router(req,res);
+// });
 
-server.listen(port, () => {
-	console.log(`Server running`);
-});
+// server.listen(port, () => {
+// 	console.log(`Server running`);
+// });
+
+
+const cluster = require('cluster');
+const cpuNum = require('os').cpus().length;
+
+//创建cluster集群
+if (cluster.isMaster) {
+  //轮叫调度
+  cluster.schedulingPolicy = cluster.SCHED_RR;
+
+  let workers = {};
+
+  // 创建工作进程
+  for (let i = 0; i < cpuNum; i++) {
+    cluster.fork();    
+  }
+
+  // exit事件
+  cluster.on('exit', (worker, code) => {
+    console.info(`worker[${worker.process.pid}] exit, code: ${code}`)
+    delete workers[worker.process.pid];
+  })
+
+  // fork事件
+  cluster.on('fork', worker => {
+    workers[worker.process.pid] = worker;
+
+    // 进程通信
+    worker.on('message', info => {
+      if (info.act === 'suicide') {
+        console.info(`worker[${worker.process.pid}] suicide`);
+        cluster.fork();
+      }
+    });
+    console.info(`worker[${worker.process.pid}] fork success`);
+  });
+} else {
+  require('./worker.js');
+}
---
+++ worker.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const http = require('http');
+const mail = require('./mail.js');
+
+
+// 监听server信息
+let server = http.createServer();
+server.listen(80, () => {
+  console.log('Server running');
+});
+server.on('request', (req, res) => {
+  for(var key in require.cache){
+		if(!key.includes('node_modules')){
+			delete require.cache[key];
+		}
+  }
+  // 处理超时
+  const timerId = setTimeout(() => {
+    res.end(`processing timeout`);
+  }, 3000);
+
+	require('./router').router(req,res);
+
+  clearTimeout(timerId);
+
+  res.end(`worker[${process.pid}] handle request`);
+})
+
+// 处理未捕获的异常
+let errNum = 0;
+process.on('uncaughtException', (err) => {
+  // 限制进程频繁重启
+  if (errNum > 0) return;
+  errNum++;
+
+  console.info(`error: ${err}`);
+  process.send({act: 'suicide'});
+  mail.send('[服务器异常]' + err.toString(), err.stack)
+  
+  // 关闭进程不接受请求
+  server.close(() => {
+    process.exit(1);
+  });
+
+  // 超时处理 退出
+  setTimeout(() => {
+    process.exit(1);
+  }, 5000);
+})
