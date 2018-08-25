cdebdd222c111f9aeaa14a318890a251b50c7d01
YangMMO <11436052@qq.com>
2018-08-25 14:05:26 +0800

    push

--- server.js
+++ server.js
@@ -32,11 +32,12 @@ if (cluster.isMaster) {
         console.info(`worker[${worker.process.pid}] suicide`);
         cluster.fork();
       }
-      if (info.err.date > errDate + 10000) {
-        errDate = info.err.date;
-        console.log('err: ' + errDate)
+      console.log(info)
+      // if (info.err.date > errDate + 10000) {
+      //   errDate = info.err.date;
+        // console.log('err: ' + errDate)
         // mail.send('[服务器异常]' + err.toString(), err.stack)
-      }
+      // }
     });
     console.info(`worker[${worker.process.pid}] fork success`);
   });
